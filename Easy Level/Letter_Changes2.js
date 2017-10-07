2c06429680fa13187f79789a1b4a944c50fb8470
Javier Gonzalez <javihergonzalez@gmail.com>
2017-10-06 17:34:46 -0700

    Add files via upload

---
+++ Easy Level/Letter_Changes2.js
@@ -0,0 +1,15 @@
+
+function LetterChanges(str) { 
+    
+  var letterChanged = str.replace(/[a-z]/gi, function(letter){
+    return (letter === 'z' || letter === 'Z') ? 'a' : String.fromCharCode(letter.charCodeAt() + 1); 
+  });  
+  var capitalized = letterChanged.replace(/[a|e|i|o|u]/gi, function(vowel){
+    return vowel.toUpperCase();
+  });
+
+  return capitalized;
+         
+}
+   
+LetterChanges("This is a test para los que 3t4n v1v0s!");  
