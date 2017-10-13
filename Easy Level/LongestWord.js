99a45e7c3cc8e8ebd98c6ba0b09d5224675f878f
Javier Gonzalez <javihergonzalez@gmail.com>
2017-10-12 19:08:05 -0700

    Update LongestWord.js

--- Easy Level/LongestWord.js
+++ Easy Level/LongestWord.js
@@ -1,4 +1,5 @@
 //Longest word excluding characters that are not part of the alphabet
+var string = 'This is a string8888 but which!@#$% one is the *(^$)gest';
 
 function LongestWord(sen) { 
   var arr = sen.split('');
@@ -18,6 +19,28 @@ function LongestWord(sen) {
   }
   return longest;
 }
-   
-// keep this function call here 
-LongestWord(readline());
+LongestWord(string);
+
+/////////////////////////////////////////////////////////////////////////
+
+function eliminateUnwantedCharacteres(s) {
+  var arr = s.split('');
+  var alf = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
+  var newArr = [];
+  for (var i = 0; i < arr.length; i++) {
+      if (alf.indexOf(arr[i]) !== -1 || arr[i] === ' ') {
+          newArr.push(arr[i]);
+      }
+  }
+  return newArr.join('').split(' ');
+}
+function longestWord(array) {
+  var longest = array.shift();
+  for (var i = 0; i < array.length; i++) {
+    if (array[i].length > longest.length) {
+      longest = array[i];
+    }
+  }
+  return longest;
+}
+longestWord(eliminateUnwantedCharacteres(string));
